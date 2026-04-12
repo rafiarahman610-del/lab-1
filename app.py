c5de479ef2387866cbdc7cb0af4af687a8f059f7
rafiarahman610-del <rafiarahman610@gmail.com>
2026-04-12 14:09:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 st.title('Customer Churn Prediction System')
 @st.cache_resource
 def load_model():
-    with open("lab-1/model.pkl", "rb") as file:
+    with open("model.pkl", "rb") as file:
         model = pickle.load(file)
     return model
 
